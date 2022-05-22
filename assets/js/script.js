bdca02169f939e107a4d92b1da36501863288f28
Fajar Nur Wahid <fajarnurwahid12@gmail.com>
2022-05-22 10:57:36 +0700

    Create sidebar and commit all files

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // start: Sidebar
+    $('.sidebar-dropdown-menu').slideUp('fast')
+
+    $('.sidebar-menu-item.has-dropdown > a, .sidebar-dropdown-menu-item.has-dropdown > a').click(function(e) {
+        e.preventDefault()
+
+        if(!($(this).parent().hasClass('focused'))) {
+            $(this).parent().parent().find('.sidebar-dropdown-menu').slideUp('fast')
+            $(this).parent().parent().find('.has-dropdown').removeClass('focused')
+        }
+
+        $(this).next().slideToggle('fast')
+        $(this).parent().toggleClass('focused')
+    })
+
+    $('.sidebar-toggle').click(function() {
+        $('.sidebar').toggleClass('collapsed')
+
+        $('.sidebar.collapsed').mouseleave(function() {
+            $('.sidebar-dropdown-menu').slideUp('fast')
+            $('.sidebar-menu-item.has-dropdown, .sidebar-dropdown-menu-item.has-dropdown').removeClass('focused')
+        })
+    })
+    // end: Sidebar
+})
